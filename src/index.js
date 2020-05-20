2907b1b08050d9aa478b62865707c03967c9cc6b
George Maia <georgemaia@users.noreply.github.com>
2020-05-20 20:35:04 -0300

    react router dom

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Repositories from './Repositories';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App title="Hello" user="Gama Academy" />
-  </React.StrictMode>,
+  
+  <App />,
   document.getElementById('root')
 );
 
