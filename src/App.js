b7da940b7c209d02552c50ae98eac474d0ea06f4
George Maia <georgemaia@users.noreply.github.com>
2020-05-20 15:53:30 -0300

    Aplicação extraind repositorios do usuario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 function App(props) {
   const [ usuario, setUsuario ] = useState('');
+
+  function handlePesquisa() {
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+  }
+  
   return (
   <>
-    <p>{ usuario }</p>
-    <input className="usuarioInput" placeholder="usuário do Github" onChange={e => setUsuario(e.target.value)} />
-    <button type="button">Pesquisar</button>
+    <input className="usuarioInput" placeholder="usuário do Github" value={usuario} onChange={e => setUsuario(e.target.value)} />
+    <button type="button" onClick={handlePesquisa}>Pesquisar</button>
   </>
   );
 }
