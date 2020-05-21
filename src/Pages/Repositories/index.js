85bd9216ab39d315d388c713831b6c63ed880a77
George Maia <georgemaia@users.noreply.github.com>
2020-05-20 21:50:47 -0300

    styled-components

---
+++ src/Pages/Repositories/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Repositories() {
+    return (
+        <>
+            <h1>Repositórios</h1>
+            <ul>
+                <li>Repositório 01</li>
+                <li>Repositório 02</li>
+                <li>Repositório 03</li>
+            </ul>
+        </>
+    )
+}
